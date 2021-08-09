66298e10057aa9b9a187def1bf9215bfdef5e15e
Cyano Hao <c@cyano.cn>
2021-08-09 20:44:50 +0800

    simplify weight axis

--- font/script/rewrite-axis/noto.mjs
+++ font/script/rewrite-axis/noto.mjs
@@ -7,12 +7,17 @@ function roundTo(x, precision) {
 function rewriteAxisNoto(font) {
 	font.stat = null;
 	font.fvar.instances = [];
+
+	// match SHS
 	const dimWght = font.fvar.axes[0].dim;
-	dimWght.min = 200;  // match SHS
+	dimWght.min = 200;
+	dimWght.default = 200;
+	dimWght.max = 900;
 	font.avar.segmentMaps.set(dimWght, [
 		[-1, -1],
-		[-0.5, -0.625],
 		[0, 0],
+		[1/7, 0.15],
+		[2/7, 0.4],
 		[1, 1],
 	]);
 
@@ -25,32 +30,20 @@ function rewriteAxisNoto(font) {
 	]);
 
 	// min reset, rectify all data
-	const instanceNewWghtNeg = new Map([[dimWght, -0.8], [dimWdth, 0]]);
+	const instanceNewOrigin = new Map([[dimWght, -0.8], [dimWdth, 0]]);
 	const instanceNewWghtPos = new Map([[dimWght, 1], [dimWdth, 0]]);
-	const instanceNewWdthNeg = new Map([[dimWght, 0], [dimWdth, -0.7]]);
-	const instanceNewWgthNegWdthNeg = new Map([[dimWght, -0.8], [dimWdth, -0.7]]);
+	const instanceNewWdthNeg = new Map([[dimWght, -0.8], [dimWdth, -0.7]]);
 	const instanceNewWgthPosWdthNeg = new Map([[dimWght, 1], [dimWdth, -0.7]]);
 
 	const masterSet = new Ot.Var.MasterSet();
-	const masterWghtNeg = new Ot.Var.Master([
-		{ dim: dimWght, min: -1, peak: -1, max: 0, },
-		{ dim: dimWdth, min: -1, peak: 0, max: 0, },
-	]);
-	masterSet.getOrPush(masterWghtNeg);
 	const masterWghtPos = new Ot.Var.Master([
 		{ dim: dimWght, min: 0, peak: 1, max: 1, },
-		{ dim: dimWdth, min: -1, peak: 0, max: 0, },
 	]);
 	masterSet.getOrPush(masterWghtPos);
 	const masterWdthNeg = new Ot.Var.Master([
 		{ dim: dimWdth, min: -1, peak: -1, max: 0, },
 	]);
 	masterSet.getOrPush(masterWdthNeg);
-	const masterWghtNegWdthNeg = new Ot.Var.Master([
-		{ dim: dimWght, min: -1, peak: -1, max: 0, },
-		{ dim: dimWdth, min: -1, peak: -1, max: 0, },
-	]);
-	masterSet.getOrPush(masterWghtNegWdthNeg);
 	const masterWghtPosWdthNeg = new Ot.Var.Master([
 		{ dim: dimWght, min: 0, peak: 1, max: 1, },
 		{ dim: dimWdth, min: -1, peak: -1, max: 0, },
@@ -60,17 +53,13 @@ function rewriteAxisNoto(font) {
 
 	const recCoord = {
 		coord: function (x) {
-			const newOrigin = roundTo(Ot.Var.Ops.originOf(x), 1);
-			const newWghtNeg = roundTo(Ot.Var.Ops.evaluate(x, instanceNewWghtNeg), 1);
+			const newOrigin = roundTo(Ot.Var.Ops.evaluate(x, instanceNewOrigin), 1);
 			const newWghtPos = roundTo(Ot.Var.Ops.evaluate(x, instanceNewWghtPos), 1);
 			const newWdthNeg = roundTo(Ot.Var.Ops.evaluate(x, instanceNewWdthNeg), 1);
-			const newWgthNegWdthNeg = roundTo(Ot.Var.Ops.evaluate(x, instanceNewWgthNegWdthNeg), 1);
 			const newWgthPosWdthNeg = roundTo(Ot.Var.Ops.evaluate(x, instanceNewWgthPosWdthNeg), 1);
 			return valueFactory.create(newOrigin, [
-				[masterWghtNeg, newWghtNeg - newOrigin],
 				[masterWghtPos, newWghtPos - newOrigin],
 				[masterWdthNeg, newWdthNeg - newOrigin],
-				[masterWghtNegWdthNeg, newWgthNegWdthNeg - newWghtNeg - newWdthNeg + newOrigin],
 				[masterWghtPosWdthNeg, newWgthPosWdthNeg - newWghtPos - newWdthNeg + newOrigin],
 			]);
 		},
--- font/script/rewrite-axis/shs.mjs
+++ font/script/rewrite-axis/shs.mjs
@@ -8,15 +8,15 @@ function rewriteAxisShs(font) {
 	font.stat = null;
 	font.fvar.instances = [];
 
-	// match Noto Sans
+	// follow Nowar Sans
 	const dimWght = font.fvar.axes[0].dim;
 	dimWght.min = 200;
-	dimWght.default = 400;
-	dimWght.max = 900;
+	dimWght.default = 200;
 	font.avar.segmentMaps.set(dimWght, [
 		[-1, -1],
-		[-0.5, -0.625],
 		[0, 0],
+		[1/7, 0.15],
+		[2/7, 0.4],
 		[1, 1],
 	]);
 
@@ -29,42 +29,6 @@ function rewriteAxisShs(font) {
 		[0, 0],
 		[1, 1],
 	]);
-
-	// origin reset, rectify all data
-	const instanceNewOrigin = new Map([[dimWght, 0.4]]);  // follow Nowar Sans
-	const instanceNewPositive = new Map([[dimWght, 1]]);
-
-	const masterSet = new Ot.Var.MasterSet();
-	const masterNegative = new Ot.Var.Master([{
-		dim: dimWght,
-		min: -1,
-		peak: -1,
-		max: 0,
-	}]);
-	masterSet.getOrPush(masterNegative);
-	const masterPositive = new Ot.Var.Master([{
-		dim: dimWght,
-		min: 0,
-		peak: 1,
-		max: 1,
-	}]);
-	masterSet.getOrPush(masterPositive);
-	const valueFactory = new Ot.Var.ValueFactory(masterSet);
-
-	const recCoord = {
-		coord: function (x) {
-			const newNegative = roundTo(Ot.Var.Ops.originOf(x), 1);
-			const newOrigin = roundTo(Ot.Var.Ops.evaluate(x, instanceNewOrigin), 1);
-			const newPositive = roundTo(Ot.Var.Ops.evaluate(x, instanceNewPositive), 1);
-			return valueFactory.create(newOrigin, [
-				[masterNegative, newNegative - newOrigin],
-				[masterPositive, newPositive - newOrigin],
-			]);
-		},
-		cv: x => x,
-	};
-
-	Rectify.inPlaceRectifyFontCoords(recCoord, Rectify.IdPointAttachmentRectifier, font);
 }
 
 export { rewriteAxisShs };
